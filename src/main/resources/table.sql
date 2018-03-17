f899b70d5fd838e7a37b1a05db9e60a277201d9c
zhouguanya <zhouguanya@meituan.com>
2018-03-17 10:28:51 +0800

    修改SQL，插入初始化数据

--- src/main/resources/table.sql
+++ src/main/resources/table.sql
@@ -1,29 +1,34 @@
-# 建数据库
+-- 建数据库
 CREATE DATABASE IF NOT EXISTS `message_system` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-# 使用数据库message_system
+-- 使用数据库message_system
 USE `message_system`;
 
-# 删除表
+-- 删除表
 DROP TABLE IF EXISTS `producer`;
+DROP TABLE IF EXISTS `consumer`;
+DROP TABLE IF EXISTS `destination`;
+DROP TABLE IF EXISTS `producer_destination`;
+DROP TABLE IF EXISTS `consumer_destination`;
 
-# 建表语句
+
+-- 建表语句
 CREATE TABLE IF NOT EXISTS `producer` (
   `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '主键id',
-  `name` VARCHAR(20) NOT NULL COMMENT '生产者名',
+  `name` VARCHAR(64) NOT NULL COMMENT '生产者名',
   `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间'
 )ENGINE = InnoDB DEFAULT CHARSET = utf8 COMMENT '生产者信息表';
 
 CREATE TABLE IF NOT EXISTS `consumer` (
   `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '主键id',
-  `name` VARCHAR(20) NOT NULL COMMENT '消费者名',
+  `name` VARCHAR(64) NOT NULL COMMENT '消费者名',
   `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间'
 )ENGINE = InnoDB DEFAULT CHARSET = utf8 COMMENT '消费者信息表';
 
 CREATE TABLE IF NOT EXISTS `destination` (
   `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '主键id',
-  `name` VARCHAR(20) NOT NULL COMMENT 'destination名',
+  `name` VARCHAR(64) NOT NULL COMMENT 'destination名',
   `type` INT NOT NULL DEFAULT 0 COMMENT '类型 0：topic 1：queue',
   `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间'
@@ -44,3 +49,18 @@ CREATE TABLE IF NOT EXISTS `consumer_destination` (
   `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间'
 )ENGINE = InnoDB DEFAULT CHARSET = utf8 COMMENT '消费者destination关系信息表';
+
+-- 初始化数据
+-- 生产者名称规则：系统名_生产者名_producer_编号
+-- 插入默认生产者
+INSERT INTO `producer`(`name`) VALUES ('default_default_producer_001');
+-- 消费者名称规则：系统名_消费者名_consumer_编号
+-- 插入默认消费者
+INSERT INTO `consumer`(`name`) VALUES ('default_default_consumer_001');
+-- destination名称规则：系统名_destination名_destination_编号
+-- 插入默认destination
+INSERT INTO `destination`(`name`) VALUES ('default_default_destination_001');
+-- 默认生产者destination关系
+INSERT INTO `producer_destination`(`producer_id`, `destination_id`) VALUES ('1', '1');
+-- 默认消费者destination关系
+INSERT INTO `consumer_destination`(`consumer_id`, `destination_id`) VALUES ('1', '1');
